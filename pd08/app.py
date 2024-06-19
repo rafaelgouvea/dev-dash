6ae52ba938d9f587abd3cdda8410b01c6e779c5e
rafaelgouvea <rafael.gouveasouza@gmail.com>
2024-06-19 06:35:50 +0000

    Cria barra lateral na UI

--- pd08/app.py
+++ pd08/app.py
@@ -2,7 +2,28 @@ from shiny import App, render, ui
 
 #Interface do usuário ----
 app_ui = ui.page_navbar(
-    ui.nav("Página 1"),
+    ui.nav(
+        "Página 1",
+        ui.layout_sidebar(
+            ui.panel_sidebar(
+                ui.markdown(
+                    """
+                    Um testo em **negrito** ou em *itálico*
+
+                    Um parágrafo com um [link](https://www.afirainvestimentos.com/)
+
+                    - Item 1
+                    - Item 2
+                    - Item 3
+
+                    ![](https://static.wixstatic.com/media/761fe6_9a3b9a5f05d946b7951017dfed5fac4a~mv2.png/v1/fill/w_160,h_68,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo_Completo_Chumbo.png)
+                    """
+                ),
+                width = 3
+            ),
+            ui.panel_main()
+        )
+        ),
     ui.nav("Página 2"),
     ui.nav_control(ui.a("ÁFIRA INVESTIMENTOS", href = "https://www.afirainvestimentos.com/")),
     ui.nav_menu(
@@ -11,11 +32,12 @@ app_ui = ui.page_navbar(
         ui.nav_control(ui.a("ÁFIRA GESTÃO", href = "https://www.afirainvestimentos.com/wealth-management"))
         ),
     title = ui.row(
-        ui.column(3, ui.img(src = "https://static.wixstatic.com/media/761fe6_9a3b9a5f05d946b7951017dfed5fac4a~mv2.png/v1/fill/w_160,h_68,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo_Completo_Chumbo.png")),
+        ui.column(3, ui.img(src = "https://static.wixstatic.com/media/761fe6_9a3b9a5f05d946b7951017dfed5fac4a~mv2.png/v1/fill/w_160,h_68,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Logo_Completo_Chumbo.png",
+                            width = 70)),
         ui.column(9, "Título da Dashboard")
     ),
-    bg = "blue",
-    inverse= True
+    bg = " #F6EFE2",
+    inverse= False
 )
 
 
