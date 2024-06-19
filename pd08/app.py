ff35ca03a510fca3f680732d3796a81d3c210a54
rafaelgouvea <rafael.gouveasouza@gmail.com>
2024-06-19 06:53:38 +0000

    Cria blocos de conteúdo no painel principal da UI

--- pd08/app.py
+++ pd08/app.py
@@ -23,6 +23,15 @@ app_ui = ui.page_navbar(
             ),
             ui.panel_main(
                 "Painel de Conteúdo Principal",
+                ui.row(
+                    ui.column(6, "Linha 1 - Coluna A", style = "background-color: red;"),
+                    ui.column(6, "Linha 1 - Coluna B", style = "background-color: purple;")
+                    ),
+                ui.row(
+                    ui.column(4, "Linha 2 - Coluna C", style = "background-color: yellow;"),
+                    ui.column(4, "Linha 2 - Coluna D", style = "background-color: black;"),
+                    ui.column(4, "Linha 2 - Coluna E", style = "background-color: orange;")
+                    ),
                 style = "background-color: #B7B7A5"
                 )
         )
